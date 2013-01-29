7629a131f3f230f2e9e938c1707f89f324cb3f95
Luca Pireddu <pireddu@crs4.it>
2013-01-29 15:31:04 +0100

    Add support for new demux option --no-index

--- seal/demux_galaxy.py
+++ seal/demux_galaxy.py
@@ -25,30 +25,54 @@ import sys
 
 # XXX: add --append-python-path to the possible arguments?
 
+def parse_indexed(s):
+  if s is not None:
+    normalized = s.lower().strip()
+    if normalized == 'notindexed':
+      return False
+    elif normalized == 'indexed':
+      return True
+  return None # failed to parse
+
+def parse_index_present(param):
+  is_indexed = parse_indexed(param)
+  if is_indexed is None:
+    # try to read it as a file
+    if os.path.isfile(param):
+      with open(param) as f:
+        contents = f.readline(10000)
+        uri, value = contents.split("\t", 1)
+        is_indexed = parse_indexed(value)
+        if is_indexed is None:
+          raise RuntimeError("Error determining whether run has an index read. " + \
+              "Couldn't parse the dataset that was supposed to specify it (first 1000 chars): %s" % contents)
+  return is_indexed
+
 def usage_error(msg=None):
   print >> sys.stderr, "Usage error"
   if msg:
     print >> sys.stderr, msg
   print >> sys.stderr, "Usage:", os.path.basename(sys.argv[0]),\
-    "GALAXY_DATA_INDEX_DIR INPUT_DATA MISMATCHES NEW_FILE_PATH NUM_REDUCERS OUTPUT1 OUTPUT_ID SAMPLE_SHEET INPUT_FORMAT OUTPUT_FORMAT OUTPUT_COMPRESSION"
+    "GALAXY_DATA_INDEX_DIR INPUT_DATA MISMATCHES NEW_FILE_PATH NUM_REDUCERS OUTPUT1 OUTPUT_ID SAMPLE_SHEET INPUT_FORMAT OUTPUT_FORMAT OUTPUT_COMPRESSION INDEX_PRESENT"
   sys.exit(1)
 
 
 if __name__ == "__main__":
-  if len(sys.argv) != 12:
+  if len(sys.argv) != 13:
     usage_error()
 
   galaxy_data_index_dir = sys.argv[1]
-  input_data = sys.argv[2]
-  mismatches = sys.argv[3]
-  new_file_path = sys.argv[4]
-  num_reducers = sys.argv[5]
-  output1 = sys.argv[6]
-  output_id = sys.argv[7]
-  sample_sheet = sys.argv[8]
-  input_format = sys.argv[9]
-  output_format = sys.argv[10]
+  input_data         = sys.argv[2]
+  mismatches         = sys.argv[3]
+  new_file_path      = sys.argv[4]
+  num_reducers       = sys.argv[5]
+  output1            = sys.argv[6]
+  output_id          = sys.argv[7]
+  sample_sheet       = sys.argv[8]
+  input_format       = sys.argv[9]
+  output_format      = sys.argv[10]
   output_compression = sys.argv[11]
+  index_present      = sys.argv[12]
 
   mydir = os.path.abspath(os.path.dirname(__file__))
 
@@ -75,6 +99,10 @@ if __name__ == "__main__":
 
   if mismatches != '0':
     cmd.extend( ('--mismatches', mismatches) )
+
+  is_indexed = parse_index_present(index_present)
+  if is_indexed is False:
+    cmd.append("--no-index")
 
   print >> sys.stderr, ' '.join(cmd)
   subprocess.check_call(cmd)
