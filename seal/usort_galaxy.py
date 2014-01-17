ba15701bcf2263542f8fb6ee741410e5aa68bd49
Luca Pireddu <pireddu@crs4.it>
2014-01-17 12:23:15 +0100

    Add license

--- seal/usort_galaxy.py
+++ seal/usort_galaxy.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+# Copyright (C) 2011-2014 CRS4.
+#
+# This file is part of Seal.
+#
+# Seal is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Seal is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Seal.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 """
 Calls the Seal USort tool.  Then, it performs the custom galaxy integration
 separating the output files into two data sets -- one for read 1, one for read 2.
