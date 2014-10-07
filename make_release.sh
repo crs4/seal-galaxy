c842e099eec2901dcfc60c90d0e625dea5b85c8b
Luca Pireddu <pireddu@crs4.it>
2014-10-07 16:08:42 +0200

    More advanced wrapper release script

--- make_release.sh
+++ make_release.sh
@@ -6,44 +6,6 @@ set -o nounset
 
 PackageName="seal-galaxy"
 
-function rewrite_seal_version() {
-	local seal_rev="${1}"
-	local short_rev="${seal_rev::8}"
-	local version_str="master_${short_rev}"
-	echo "Will rewrite tool_dependencies.xml setting the the package version to '${version_str}'."
-	echo "Are you sure you want to proceed? [Y/n]"
-	read -p "Answer: " yn
-	case "${yn}" in
-			''|[Yy]) # do nothing and keep going
-					;;
-			[Nn]) echo "Aborting"; exit 0
-					;;
-			*) usage_error "Unrecognized answer. Please specify Y or n"
-					;;
-	esac
-
-	local grep_expr='<package name="seal" version=".*">'
-	if ! grep  "${grep_expr}" tool_dependencies.xml >/dev/null ; then
-			error "Couldn't find expected package line in tool_dependencies.xml"
-	fi
-
-	printf -v sed_expr1  '/<package name="seal"/s/version="[^"]*"/version="%s"/' "${version_str}"
-	printf -v sed_expr2  '/<action type="shell_command">/s/git reset --hard \([^<]\+\)\s*/git reset --hard %s/' "${seal_rev}"
-	sed -i -e "${sed_expr1}" -e "${sed_expr2}" tool_dependencies.xml
-	echo "Edited tool_dependencies.xml"
-
-	# edit the tools as well
-	printf -v sed_expr3 's|<requirement type="package" version="[^"]\+">seal</requirement>|<requirement type="package" version="%s">seal</requirement>|' "${version_str}"
-	sed -i -e "${sed_expr3}" seal/*.xml
-	echo "Edited tool definitions"
-}
-
-function make_archive() {
-	local archive_name=${PackageName}.tar.gz
-	tar czf "${archive_name}" seal tool_dependencies.xml tool_data_table_conf.xml.sample README.md
-	echo "Created new archive ${archive_name}"
-}
-
 
 function error() {
     if [ $# -ge 1 ]; then
@@ -56,18 +18,68 @@ function usage_error() {
     error  "Usage: $0 [ Seal revid ]"
 }
 
-if [ $# -gt 2 ]; then
+function confirm() {
+    local prompt="${1}"
+    echo "${prompt} [Y/n]"
+    read -p "Answer: " yn
+    case "${yn}" in
+        ''|[Yy]) # do nothing and keep going
+            ;;
+        [Nn]) echo "Aborting"; exit 0
+            ;;
+        *) usage_error "Unrecognized answer. Please specify Y or n"
+            ;;
+    esac
+    return 0
+}
+
+function rewrite_seal_version() {
+	local grep_expr='<package name="seal" version=".*">'
+	if ! grep  "${grep_expr}" tool_dependencies.xml >/dev/null ; then
+			error "Couldn't find expected package line in tool_dependencies.xml"
+	fi
+
+	printf -v sed_expr1  '/<package name="seal"/s/version="[^"]*"/version="%s"/' "${seal_version}"
+	printf -v sed_expr2  '/<action type="shell_command">/s/git reset --hard \([^<]\+\)\s*/git reset --hard %s/' "${seal_version}"
+	sed -i -e "${sed_expr1}" -e "${sed_expr2}" tool_dependencies.xml
+	echo "Edited tool_dependencies.xml" >&2
+
+	# edit the tools as well
+	printf -v sed_expr3 '/<requirement type="package" version=.*>\s*seal\s*</s/version="[^"]\+"/version="%s"/' "${seal_version}"
+	printf -v sed_expr4 '/<tool id=/s/version="[^"]\+"/version="%s"/' "${seal_version}"
+	sed -i -e "${sed_expr3}" -e "${sed_expr4}" seal/*.xml
+
+	echo "Edited tool definitions" >&2
+}
+
+############# main ###############3
+
+if [ $# -eq 1 ]; then
+    seal_version="${1}"
+else
     usage_error
-elif [ $# -eq 1 ]; then
-    seal_rev="${1}"
-else # $# equals 0
-    seal_rev=""
 fi
 
-if [ -n "${seal_rev}" ]; then
-	rewrite_seal_version "${seal_rev}"
+echo "Will rewrite tool_dependencies.xml setting the the package version to '${seal_version}'."
+confirm "Are you sure you want to proceed? [Y/n]"
+
+# ensure the tag doesn't already exist
+if git tag -l | grep -w "${seal_version}" ; then
+    error "A release tag called '${seal_version}' already exists"
 fi
 
-make_archive
+rewrite_seal_version "${seal_version}"
 
-echo "Don't forget to commit and tag as necessary and then upload the archive to the toolshed!"
+git commit -a -m "Wrappers release for Seal '${seal_version}'"
+git tag "${seal_version}"
+
+revid=$(git rev-parse HEAD)
+
+echo "Tagged new commit ${revid} with tag '${seal_version}'"
+
+short_revid=${revid::8}
+archive_name=${PackageName}-${short_revid}.tar.gz
+
+git archive --format tar.gz --prefix ${PackageName}-${short_revid}/ HEAD -o "${archive_name}"
+
+echo "Don't forget to upload the archive to the toolshed!"
